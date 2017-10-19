f13b2429b27d86369662df6b20216a9948f8d937
Alex Harvey <alex@razorconsulting.com.au>
2017-10-20 02:28:13 +1100

    A bunch of changes to the kibana class
    I had these changes unsaved and I can't remember what the reason for
    them was. However I found they were necessary to get compilation to
    succeed.

--- site/modules/profile/manifests/kibana4.pp
+++ site/modules/profile/manifests/kibana4.pp
@@ -1,28 +1,28 @@
 class profile::kibana4 (
   Hash $firewall_multis,
-  String $package_download_url,
   Integer[30000] $uid,
   Integer[30000] $gid,
-  Hash $config,
 ) {
   # Pass in an empty hash here if used in conjunction with
   # profile::nginx.
   create_resources(firewall_multi, $firewall_multis)
 
-  include profile::jdk
+  # Manage the user and group to prevent random UID and
+  # GID assignment by the RPM.
 
-  class { 'kibana4':
-    package_provider     => 'archive',
-    archive_provider     => 'camptocamp',
-    package_download_url => $package_download_url,
-    package_dl_timeout   => 0,
-    manage_user          => true,
-    manage_init_file     => true,
-    service_name         => 'kibana',
-    kibana4_user         => 'kibana',
-    kibana4_group        => 'kibana',
-    kibana4_uid          => $uid,
-    kibana4_gid          => $gid,
-    config               => $config,
+  group { 'kibana':
+    ensure => present,
+    gid    => $gid,
   }
+  user { 'kibana':
+    ensure     => present,
+    uid        => $uid,
+    gid        => $gid,
+    home       => '/home/kibana',
+    shell      => '/bin/bash',
+    managehome => false,
+  }
+
+  include profile::jdk
+  include kibana4
 }
