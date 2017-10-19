5aa7899c5b0f0040e87b43735fe42c137dda49eb
Alex Harvey <alex@razorconsulting.com.au>
2017-10-20 02:29:29 +1100

    Add some missing facts
    A bunch of facts added to spec_helper.rb that weren't required before.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,11 +10,14 @@ RSpec.configure do |c|
     :concat_basedir            => '/var/lib/puppet/concat',
     :espv                      => '/dev/sdc',
     :kernel                    => 'Linux',
+    :id                        => '1',
+    :is_virtual                => true,
     :operatingsystem           => 'RedHat',
     :operatingsystemmajrelease => '7',
     :operatingsystemrelease    => '7.2.1511',
     :osfamily                  => 'RedHat',
     :path                      => ['/bin', '/usr/bin'],
     :redispv                   => '/dev/sdb',
+    :selinux                   => false,
   }
 end
