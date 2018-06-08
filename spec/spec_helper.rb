b94a0bcff38987221914c5f3d0ce7d052586418d
Alex Harvey <alex@razorconsulting.com.au>
2018-06-08 21:45:26 +1000

    Update spec_helper.rb
    Remove old form of boilerplate from spec/spec_helper.rb, and also
    configure colouring and documentation format.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,22 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-
 RSpec.configure do |c|
-  c.module_path     = File.join(fixture_path, 'modules')
-  c.hiera_config    = File.join(fixture_path, 'hiera.yaml')
-  c.environmentpath = File.join(Dir.pwd, 'spec')
+  c.color        = true
+  c.hiera_config = 'spec/fixtures/hiera.yaml'
+  c.formatter    = :documentation
+
   c.default_facts   = {
-    :concat_basedir            => '/var/lib/puppet/concat',
-    :espv                      => '/dev/sdc',
-    :kernel                    => 'Linux',
-    :id                        => '1',
-    :is_virtual                => true,
-    :operatingsystem           => 'RedHat',
+    :concat_basedir  => '/var/lib/puppet/concat',
+    :espv            => '/dev/sdc',
+    :kernel          => 'Linux',
+    :id              => '1',
+    :is_virtual      => true,
+    :operatingsystem => 'RedHat',
     :operatingsystemmajrelease => '7',
     :operatingsystemrelease    => '7.2.1511',
-    :osfamily                  => 'RedHat',
-    :path                      => ['/bin', '/usr/bin'],
-    :redispv                   => '/dev/sdb',
-    :selinux                   => false,
+    :osfamily        => 'RedHat',
+    :path            => ['/bin', '/usr/bin'],
+    :redispv         => '/dev/sdb',
+    :selinux         => false,
   }
 end
