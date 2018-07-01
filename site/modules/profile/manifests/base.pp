ec14fa736acf835e2cf251e9e17f68605f67105b
Alex Harvey <alex@razorconsulting.com.au>
2018-07-01 21:17:49 +1000

    Add vim and other tools to profile::base
    For convenience but not for use in production.

--- site/modules/profile/manifests/base.pp
+++ site/modules/profile/manifests/base.pp
@@ -1,7 +1,10 @@
 class profile::base (
   Hash $firewall_multis,
+  Hash $tools,
 ) {
   create_resources(firewall_multi, $firewall_multis)
+  create_resources(package, $tools)
+
   include profile::base::firewall
   include profile::base::yum
   include ntp
