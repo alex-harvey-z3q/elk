6e521e8eeb542d7d03ed58214f8677e62c584136
Alex Harvey <alex@razorconsulting.com.au>
2018-07-01 23:41:54 +1000

    Further changes to ordering
    A clearer statement of the required ordering of things for the cluster
    to start up cleanly.

--- site/modules/role/manifests/elk_stack.pp
+++ site/modules/role/manifests/elk_stack.pp
@@ -22,58 +22,44 @@ class role::elk_stack {
     refreshonly       => true,
   }
 
-  # On one occasion only I saw a slow-starting Redis cause the Beaker tests
-  # to fail, and it looks like Redis needs to have started before Logstash.
+  $cluster_name = $::profile::elasticsearch::data_node::config['cluster.name']
+
+  # The order in which things need to start to ensure that everything
+  # starts cleanly.
   #
-  wait_for { 'redis':
-    query => 'cat /var/log/redis/redis.log 2> /dev/null',
-    regex => 'The server is now ready to accept connections on port 6379',
-  }
-
-  Service['redis']
+  Service["elasticsearch-instance-${cluster_name}"]
   ~>
-  Wait_for['redis']
-
-  # I am not at this point sure why, but it seems that the Logstash service can
-  # take quite a while to start. To avoid Beaker tests failing due to a
-  # slow-starting Logstash service, this wait_for pauses the Puppet run until
-  # the Logstash service actually started.
-  #
-  wait_for { 'logstash':
-    query => 'cat /var/log/logstash/logstash-plain.log 2> /dev/null',
-    regex => 'Successfully started Logstash API endpoint',
-  }
-
-  Service['logstash']
-  ~>
-  Wait_for['logstash']
-
-  # In a single node configuration with ES master and client instances, the
-  # first to start will take port 9000.  Similarly, Kibana4 isn't happy unless
-  # the ES cluster has started first. So we need to ensure that the master ES
-  # starts before the client.
-
   wait_for { 'es-master':
     query => 'cat /var/log/elasticsearch/es01/es01.log 2> /dev/null',
     regex => 'o.e.n.Node.*started',
   }
-
+  ->
+  Service["elasticsearch-instance-${cluster_name}-client-instance"]
+  ~>
   wait_for { 'es-client':
     query => 'cat /var/log/elasticsearch/es01-client-instance/es01.log 2> /dev/null',
     regex => 'o.e.n.Node.*started',
   }
-
-  $cluster_name = $::profile::elasticsearch::data_node::config['cluster.name']
-
-  Service["elasticsearch-instance-${cluster_name}"]
-  ~>
-  Wait_for['es-master']
-  ->
-  Service["elasticsearch-instance-${cluster_name}-client-instance"]
-  ~>
-  Wait_for['es-client']
   ->
   Service['kibana']
+
+  Wait_for['es-master'] -> Service['logstash']
+
+  Service['redis']
+  ~>
+  wait_for { 'redis':
+    query => 'cat /var/log/redis/redis.log 2> /dev/null',
+    regex => 'The server is now ready to accept connections on port 6379',
+  }
+  ->
+  Service['logstash']
+  ~>
+  wait_for { 'logstash':
+    query => 'cat /var/log/logstash/logstash-plain.log 2> /dev/null',
+    regex => 'Successfully started Logstash API endpoint',
+  }
+  ->
+  Service['filebeat']
 
   # This seems to be the best way to set index.number_of_replicas to 0 for
   # all indices, as required in an all-in-one configuration.
