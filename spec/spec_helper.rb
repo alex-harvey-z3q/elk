e49d3ee9a1e176562ed146edb9872b6a4d0764bd
Alex Harvey <alexharv074@gmail.com>
2019-06-13 11:27:56 +1000

    Remove unused fact mocks
    I tried excluding each fact in spec_helper.rb to see which ones are
    really required and documented which module they're actually required
    by.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,23 +21,21 @@ RSpec.configure do |c|
   c.mock_framework = :rspec
 
   c.default_facts   = {
-    :concat_basedir  => '/var/lib/puppet/concat',
+    # required by our roles & profiles.
     :espv            => '/dev/sdc',
-    :id              => '1',
-    :is_virtual      => true,
-    :kernel          => 'Linux',
-    :operatingsystem => 'RedHat',
-    :operatingsystemmajrelease => '7',
-    :operatingsystemrelease    => '7.2.1511',
-    :os => {'family' => 'RedHat', 'release' => {'major' => '7', 'minor' => '1', 'full' => '7.1.1503'}},
-    :osfamily        => 'RedHat',
-    :path            => ['/bin', '/usr/bin'],
-    :puppetversion   => '5.5.1',
     :redispv         => '/dev/sdb',
+    # firewall module.
+    :puppetversion   => '5.5.1',
     :selinux         => false,
+    :kernel          => 'Linux',
+    :osfamily        => 'RedHat',
+    :operatingsystem => 'RedHat',
+    :operatingsystemrelease => '7.2.1511',
+    # redis module.
+    :operatingsystemmajrelease => '7',
+    # filebeat, disable_transparent_hugepage etc.
+    :os => {'family' => 'RedHat', 'release' => {'major' => '7', 'minor' => '1', 'full' => '7.1.1503'}},
+    # filebeat module.
     :filebeat_version => '6',
-    :hostname        => 'myhost',
-    :domain          => 'example.com',
-    :fqdn            => 'myhost.example.com',
   }
 end
